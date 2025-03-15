10197e32528e490d1a7cc233a1cf6db00b0cfd6b
federico <federicoberto708@gmail.com>
2025-03-15 18:46:30 +0700

    refactor(db) : add log for better information

--- db/conn.go
+++ db/conn.go
@@ -19,11 +19,13 @@ func DBConnection() *sqlx.DB {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		host, username, password, database, port)
 
+	log.Print("connecting to database...")
 	db, err := sqlx.Connect("postgres", dsn)
 	if err != nil {
-		log.Fatalf("failed to connect to database : %e", err)
+		log.Fatalf("failed to connect to database : %s", err.Error())
 		return nil
 	}
+	log.Print("success connecting to database...")
 
 	return db
 }
